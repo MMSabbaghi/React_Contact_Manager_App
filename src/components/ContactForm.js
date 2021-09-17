dbe2481566e81ba31a1be8ace9e5ab3031eb51ad
Your Name <you@example.com>
2021-09-17 07:52:25 +0430

    add form validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,23 +5,50 @@ import { BiLeftArrowAlt } from "react-icons/bi";
 
 const ContactForm = ({ location, history }) => {
   const selectedContact = location.state;
-
   const initialContact = { email: "", name: "" };
   const [contact, setContact] = useState(initialContact);
+  const [errors, setErrors] = useState(null);
 
   useEffect(() => {
     if (selectedContact) setContact(selectedContact);
   }, [selectedContact]);
 
+  const validate = (values) => {
+    const name = String(values.name);
+    const email = String(values.email);
+    const errors = {};
+
+    if (name.length === 0) errors.name = "Name is required";
+    if (name.length > 0 && name.length < 3)
+      errors.name = "Contact name must be at least 3 characters long";
+
+    if (email.length === 0) errors.email = "Email is required";
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!emailRegex.test(email.toLowerCase()))
+      errors.email = "This email is invalid";
+
+    return errors;
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    selectedContact ? updateContact(contact) : addNewContact(contact);
-    history.push("/");
-    setContact(initialContact);
+    const validationErrors = validate(contact);
+
+    if (validationErrors.name || validationErrors.email) {
+      setErrors(validationErrors);
+    } else {
+      selectedContact ? updateContact(contact) : addNewContact(contact);
+      history.push("/");
+      setContact(initialContact);
+    }
   };
 
   const changeInputHandler = (e) => {
-    setContact({ ...contact, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+    //Updates the error message if it already exists
+    if (errors?.name || errors?.email) setErrors(validate(contact));
+    setContact({ ...contact, [name]: value });
   };
 
   return (
@@ -37,14 +64,16 @@ const ContactForm = ({ location, history }) => {
           onChange={changeInputHandler}
           className="text_input"
         />
+        {errors?.name && <p className="error_msg"> {errors.name} </p>}
         <label htmlFor="email">email</label>
         <input
-          type="email"
+          type="text"
           name="email"
           value={contact.email}
           onChange={changeInputHandler}
           className="text_input"
         />
+        {errors?.email && <p className="error_msg"> {errors.email} </p>}
         <button type="submit">
           {selectedContact ? "Edit" : "Add"} contact
         </button>
